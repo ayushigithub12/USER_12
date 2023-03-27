2350b458e13618f7459dca7870acb311a7551c18
ssangram111 <sangram.singh@cognicsys.com>
2023-03-27 14:36:34 +0530

    check

--- model/user.js
+++ model/user.js
@@ -1,48 +1,49 @@
-const mongoose=require('mongoose');
-const bcrypt=require('bcryptjs');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
-const userSchema=new mongoose.Schema({
-   name:{
-    type:String,
-    required:true
-   },
-   email:{
-    type:String,
-    required:true,
-    unique:true
-   },
-   contact:{
-    type:Number,
-    required:true,
-    unique:true,
-    minlength:10
-   },
-   ward:{
-    type:String,
-    required:true
-   },
-   address:{
-    type:String,
-    required:true
-   },
-   password:{
-    type:String,
-    required:true
-   },
-   cpassword:{
-    type:String,
-    required:true
-   }
-})
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contact: {
+    type: Number,
+    required: true,
+    unique: true,
+    minlength: 10,
+  },
+  ward: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  cpassword: {
+    type: String,
+    required: true,
+  },
+});
 
 //password hash
-userSchema.pre('save',async function(next){
-   if(this.isModified('password')){
-    this.password= await bcrypt.hash(this.password,10);
-   }
-   next();
-})
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    this.cpassword = await bcrypt.hash(this.cpassword, 10);
+  }
+  next();
+});
 
-const User=new mongoose.model("User",userSchema);
+const User = new mongoose.model("User", userSchema);
 
-module.exports=User;
+module.exports = User;
